440c10f72bcb2495257984ef6672ee3de74b7877
Romjan Hossain <romjanhossainn@gmail.com>
2023-10-13 22:57:51 +0600

    coffe-backend

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res) => {
+    res.send('Apner server thik ache choltache')
+})
+
+
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.4ievbno.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+    const databaseCollection = client.db('CoffeeDB').collection('coffee');
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port,() => {
+    console.log('the server is running')
+})
